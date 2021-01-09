c1212f6812aba2bb4a00dd4907b7e0e18582dbb7
Ruslan Fedchenko <darkelt.ruslan@gmail.com>
2021-01-09 19:04:19 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2|woff|eot|ttf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[contenthash].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+        options: {
+          pretty: true,
+        },
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".scss"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.pug",
+      inject: true,
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
